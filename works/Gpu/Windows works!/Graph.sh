abac944b010ad31a28867954dfe4a9a7529836e6
DarkStar <darkstar@unisocial.net>
2021-02-24 23:23:28 -0500

    gpu ubu

---
+++ works/Gpu/Windows works!/Graph.sh
@@ -0,0 +1,1 @@
+Windows will only work with Nvidia Desktop Driver! as VFIO hides the optimus Intel iGPU interface.
---
+++ works/Gpu/linux/no-vbios.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rom=MSI-GTX1070-MaxQ.rom
+image=/var/lib/libvirt/images/ubuntu20.10.qcow2
+
+if=ide
+iso=/home/me/uni/iso/19042.631.201119-0144.20h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso
+
+
+# Refer https://github.com/saveriomiroddi/qemu-pinning for how to set your cpu affinity properly
+qemu-system-x86_64 \
+  -name "Windows10-QEMU" \
+  -machine type=q35,accel=kvm \
+  -global ICH9-LPC.disable_s3=1 \
+  -global ICH9-LPC.disable_s4=1 \
+  -enable-kvm \
+  -cpu host,kvm=off,hv_vapic,hv_relaxed,hv_spinlocks=0x1fff,hv_time,hv_vendor_id=12alphanum \
+  -smp 6,sockets=1,cores=3,threads=2 \
+  -m 8G \
+  -mem-prealloc \
+  -rtc clock=host,base=localtime \
+  -device ich9-intel-hda -device hda-output \
+  -vga none \
+  -nographic \
+  -serial none \
+  -parallel none \
+  -k en-us \
+  -spice port=5901,addr=127.0.0.1,disable-ticketing \
+  -usb \
+  -device ioh3420,bus=pcie.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root.1 \
+  -device vfio-pci,host=01:00.0,bus=root.1,addr=00.0,x-pci-sub-device-id=0x11e9,x-pci-sub-vendor-id=0x1462,multifunction=on \
+  -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE.fd \
+  -drive if=pflash,format=raw,file=WIN_VARS.fd \
+  -boot menu=on \
+  -boot order=c \
+  -drive id=disk0,if=$if,cache=none,format=qcow2,file=$image \
+\
+  -device pci-bridge,addr=12.0,chassis_nr=2,id=head.2 \
+  -device usb-tablet \
+  -net bridge,br=virbr0 -net nic,model=e1000e
